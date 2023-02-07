9dc034e0e47b629a3712bee2a0528d149a62a229
lieuwebakker <3v8z4q@gmail.com>
2023-02-07 14:54:59 +0100

    Update panadero-self.js

--- panadero-self.js
+++ panadero-self.js
@@ -9,36 +9,6 @@
 // * *  *       *     *      *   *       *  *  * * *
 
 
-class Self {
-  constructor(name, code) {
-    this.name = name;
-    this.code = code;
-  }
-}
-
-const allSelfs = [
-  new Color('brightred', '#E74C3C'),
-  new Color('soothingpurple', '#9B59B6'),
-  new Color('skyblue', '#5DADE2'),
-  new Color('leafygreen', '#48C9B0'),
-  new Color('sunkissedyellow', '#F4D03F'),
-  new Color('groovygray', '#D7DBDD'),
-];
-
-const simpleSelf = ['red','grey','white','yellow','magenta','green','blue','cyan','purple','teal'];
-
-
-exports.getRandomSelf = () => {
-  return allColors[Math.floor(Math.random() * allColors.length)];
-}
-
-exports.getSimpleRandom = () => {
-  return simpleSelf[Math.floor(Math.random() * simpleColors.length)];
-}
-
-
-
-exports.allColors = allSelfs;
 
 
 
